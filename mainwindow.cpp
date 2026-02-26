293ccd1473ead7f82e6eec34a3468b03aa04835c
rust3128 <rust3128@gmail.com>
2026-02-26 09:43:33 +0200

    Додавання іконки. Інкрементна версійність

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "version.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Kirt - MPos Log Viewer ver: " + QString(APP_VERSION_STR) + " " + QString(APP_BUILD_DATETIME));
+
 }
 
 MainWindow::~MainWindow()
